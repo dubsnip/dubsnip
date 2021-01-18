166e4bca3639731710d2c91f3aa7391e37bc8cb6
Franco Ascarate <70088727+francoasc@users.noreply.github.com>
2021-01-17 22:00:05 -0300

    heroku vercel 5th try

--- client/src/components/CheckOut/ConfirmData.js
+++ client/src/components/CheckOut/ConfirmData.js
@@ -10,27 +10,25 @@ export default function ConfirmData() {
 
   useEffect(() => {
     (async () => {
-      if (status !== "loading") {
-        const script = document.createElement("script"); //Crea un elemento html script
-        const attr_data_preference = document.createAttribute(
-          "data-preference-id"
-        ); //Crea un nodo atribute
-        const { data } = await axios.post("/mercadopago", {
-          cart,
-          orderId: order.id,
-        });
+      const script = document.createElement("script"); //Crea un elemento html script
+      const attr_data_preference = document.createAttribute(
+        "data-preference-id"
+      ); //Crea un nodo atribute
+      const { data } = await axios.post("/mercadopago", {
+        cart,
+        orderId: order.id,
+      });
 
-        attr_data_preference.value = data.id; //Le asigna como valor el id que devuelve MP
-        //Agrega atributos al elemento script
-        let hola = document.getElementById("article");
-        hola = hola.appendChild(script);
-        console.log("hola");
-        console.log(hola);
-        script.src =
-          "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
-        script.setAttributeNode(attr_data_preference);
-        //Agrega el script como nodo hijo del elemento form
-      }
+      attr_data_preference.value = data.id; //Le asigna como valor el id que devuelve MP
+      //Agrega atributos al elemento script
+      let hola = document.getElementById("article");
+      hola = hola.appendChild(script);
+      console.log("hola");
+      console.log(hola);
+      script.src =
+        "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
+      script.setAttributeNode(attr_data_preference);
+      //Agrega el script como nodo hijo del elemento form
     })();
   }, [cart, order.id, status]);
 
